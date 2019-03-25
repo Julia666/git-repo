6cd57692a14d1ca6c77d773bd77d5ab03245ee28
Julia666 <zazeralie@yandex.ru>
2019-03-25 04:55:13 +0300

    добавии whats up

--- git_hello/Program.cs
+++ git_hello/Program.cs
@@ -11,6 +11,7 @@ namespace git_hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Whats up?");
         }
     }
 }
